34aa432607e1e7e3475659966f9efe45fa880d66
trisha-del <teresiahwanjiru485@gmail.com>
2023-02-16 11:13:57 +0300

    my function

---
+++ functions.py
@@ -0,0 +1,13 @@
+def add_nums(a,b):
+    results=a+b
+    print("results")
+add_nums(a=2,b=4) 
+add_nums(a=3,b=5)  
+add_nums(a=2,b=5)
+add_nums(b=3,a=6)
+
+name2=input("enter_first name")
+name2=input("enter_secondname")
+print("name1")
+print("name2")
+print("hello+name1+name2")
