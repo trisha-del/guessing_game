dfd3c88d0b4baa5ccbbca14c5ec1c63b63030cae
trisha-del <teresiahwanjiru485@gmail.com>
2023-02-17 10:41:20 +0300

    dict2

--- dict2.py
+++ dict2.py
@@ -18,5 +18,33 @@ person ={
     }
 print(person["pets"])
 
+profile={}
+profile["username"]="user123"
+profile["age"]=20
+profile["email"]="user123@gmail.com"
+print(profile)
+
+profile={}
+def register():
+    username=input("enter username")
+    email=input("enter email")
+    password=input("enter password:")
+    profile["username"]=username
+    profile["email"]=email
+    profile["password"]=password
+register()
+def get_profile():
+    print (profile)
+def change_username():
+    new_username=input("enter new user")
+    profile["username"]=new_username
+    
+
+
+register()
+get_profile()
+
+
+
 
 
