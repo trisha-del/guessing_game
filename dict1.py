1376d365cbfb49dd03f5334199bf9511616a9de7
trisha-del <teresiahwanjiru485@gmail.com>
2023-02-17 09:29:02 +0300

    dict1

---
+++ dict1.py
@@ -0,0 +1,12 @@
+light_theme={
+    "button":"red",
+    "tittle":"blue"
+}
+dark_theme={
+    "button":"black",
+    "tittle":"white"
+}
+def average (a,b):
+    av=(a + b)/2
+    return av
+    print(av)
